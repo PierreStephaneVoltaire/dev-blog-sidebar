9018906f00abbfb1de8b8ec4a055ab5066afe115
PierreStephaneVoltaire <psvoltaire96@gmail.com>
2020-07-20 21:36:23 -0400

    added media query to hide sidebar dpending on width of the device

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <template>
   <div id="app">
-<Sidebar></Sidebar>
+    <Sidebar></Sidebar>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import Sidebar from './components/Sidebar.vue'
+  import {Component, Vue} from 'vue-property-decorator'
+  import Sidebar from './components/Sidebar.vue'
 
-@Component({
-  components: {
-    Sidebar
+  @Component({
+    components: {
+      Sidebar
+    }
+  })
+  export default class App extends Vue {
   }
-})
-export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-z-index: 2;
-  margin-top: 60px;
-  position: fixed;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    z-index: 2;
+    margin-top: 60px;
+    position: absolute;
 
-}
+  }
+
+  $breakpoint-tablet: 800px;
+  @media (max-width: $breakpoint-tablet) {
+    #app {
+      display: none;
+    }
+  }
 </style>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+  import {Component, Prop, Vue} from 'vue-property-decorator'
 
   @Component
-export default class Sidebar extends Vue {
+  export default class Sidebar extends Vue {
     @Prop() private msg!: string;
-}
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -24,7 +24,7 @@ export default class Sidebar extends Vue {
     width: 20vw;
     background: white;
     cursor: pointer;
-    box-shadow: 0 2px 1px -1px rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 1px 3px 0 rgba(0,0,0,.12);
+    box-shadow: 0 2px 1px -1px rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12);
   }
 
 </style>
