11b7e953e53276b2bf3767fa554592e6a6e27bcd
PierreStephaneVoltaire <psvoltaire96@gmail.com>
2020-08-03 00:45:01 -0400

    added temp styles

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="sidebar">
-
+<div class="section-header">
+  Sort
+</div>
     <md-divider></md-divider>
 
     <md-button class="sectionbtn"> New</md-button>
@@ -9,7 +11,9 @@
     <md-button> Popular</md-button>
     <md-divider></md-divider>
 
-    <md-button> Most Visited</md-button>
+    <div class="section-header">
+      Most Visited
+    </div>
     <md-divider></md-divider>
 
     <md-list>
@@ -17,6 +21,7 @@
         <md-icon>help</md-icon>
         <md-button class="md-list-item-text">Loading Group Name...</md-button>
       </md-list-item>
+
       <md-list-item>
         <md-icon>help</md-icon>
         <md-button class="md-list-item-text">Loading Group Name...</md-button>
@@ -27,9 +32,11 @@
       </md-list-item>
     </md-list>
     <md-divider></md-divider>
-    <md-button> Groups</md-button>
+    <div class="section-header">
+      Groups
+    </div>
     <md-divider></md-divider>
-    <md-field>
+    <md-field class="searchbar">
       <label>Group name or Topic</label>
       <md-input v-model="voice"></md-input>
       <md-icon>search</md-icon>
@@ -69,9 +76,23 @@ export default class Sidebar extends Vue {
     justify-content: left;
     box-shadow: 0 2px 1px -1px rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12);
   }
-
+  .searchbar{
+    margin: 5%;
+    width: 90%;
+  }
+.section-header{
+  height: 38px;
+  background: linear-gradient(to right, lightblue 25%, darkblue 75%);
+  padding: 10px 10px 0px 25px;
+  font-weight: bolder;
+  font-size: 1rem;
+  text-align: left;
+}
   .md-button .md-ripple{
     justify-content: left!important;
+  }
+  .md-list-item{
+    padding-left: 5%;
   }
   .md-list-item-text{
     text-align: left;
